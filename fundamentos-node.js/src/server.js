6981413e151dffc884e7c71c4d7f8c617f75b9c8
Diego Paulino <diegopaulino.dev@gmail.com>
2026-02-03 19:49:43 -0300

    refactor(fundamentos): extrai rotas do servidor e organiza estrutura do projeto

--- fundamentos-node.js/src/server.js
+++ fundamentos-node.js/src/server.js
@@ -1,54 +1,21 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto';
 import { json } from './middlewares/json.js';
-import { Database } from './middlewares/database.js';
-
-const database = new Database();
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
 
     await json(req, res)
 
-    if (method == 'GET' && url == '/users') {
-        const users = database.select('users');
-        
-        return res.end(JSON.stringify(users)) // JSON.stringify(users) | Transforma o array users em um JSON
-    }
-    if (method == 'POST' && url == '/users') {
-        const { name, email } = req.body;
-        
-        const user = {
-            id: randomUUID(),
-            name,
-            email
-        }
-    
-        database.insert('users', user);
-    
-        return res.writeHead(201).end() // writeHead() | Criação de StatusCode
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
+
+    if (route) {
+        return route.handler(req, res);
     }
 
     return res.writeHead(404).end();
 })
 
 server.listen(3333);
-
-/*
-GET => Buscar um recurso no back-end
-POST => Criar um recurso no back-end
-PUT => Atualizar um recurso no back-end
-PATCH => Atualizar um recurso especifico no back-end
-DELETE => Deletar um recurso no back-end
-*/
-
-
-// Stateful => Sempre guarda informações na memória do PC
-// Stateless => Guarda as inormações em bancos de dados, etc.
-
-// JSON => JavaScript Object Notation
-
-/*
-CABEÇALHOS (req (requisição)/res (resposta)) = Metadados
-São informações adicionais sobre como interpretar o dado que está sendo enviado ou recebido, e não sobre o dado em sí.
-*/
