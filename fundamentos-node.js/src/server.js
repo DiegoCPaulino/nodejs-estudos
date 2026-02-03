50e45bddc4c78bc1cd87209cf3e4d8b18129d9de
Diego Paulino <diegopaulino.dev@gmail.com>
2026-02-03 18:54:45 -0300

    feat(fundamentos): adiciona middleware de JSON e camada de persistência em memória

--- fundamentos-node.js/src/server.js
+++ fundamentos-node.js/src/server.js
@@ -1,22 +1,29 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
+import { Database } from './middlewares/database.js';
 
-const users = []
+const database = new Database();
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req;
 
+    await json(req, res)
+
     if (method == 'GET' && url == '/users') {
-        return res
-        .setHeader('Content-type', 'application/json') // Seta o cabeçalho para enviar JSON's 
-        .end(JSON.stringify(users)) // JSON.stringify(users) | Transforma o array users em um JSON
+        const users = database.select('users');
+        
+        return res.end(JSON.stringify(users)) // JSON.stringify(users) | Transforma o array users em um JSON
     }
     if (method == 'POST' && url == '/users') {
-        users.push({
-        name: 'Nome Teste',
-        id: 1,
-        email: 'emailteste@teste.com.br'
-        })
+        const { name, email } = req.body;
+        
+        const user = {
+            id: 1,
+            name,
+            email
+        }
     
+        database.insert('users', user);
     
         return res.writeHead(201).end() // writeHead() | Criação de StatusCode
     }
