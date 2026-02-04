6394a4f5c2fe21c4eb5870b10f2acb71f89374ff
Diego Paulino <diegopaulino.dev@gmail.com>
2026-02-03 23:17:45 -0300

    feat(users): adiciona atualização e remoção de usuários

--- fundamentos-node.js/src/routes.js
+++ fundamentos-node.js/src/routes.js
@@ -1,11 +1,12 @@
 const database = new Database();
 import { Database } from './database.js';
 import { randomUUID } from 'node:crypto';
+import { buildRoutePath } from './utils/build_route_path.js';
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const users = database.select('users');
 
@@ -14,7 +15,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body;
                 
@@ -26,7 +27,32 @@ export const routes = [
             
                 database.insert('users', user);
             
-                return res.writeHead(201).end()
+                return res.writeHead(201).end();
         }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params;
+            const { name, email } = req.body;
+            database.update('users', id, {
+                name,
+                email,
+            });
+
+            return res.writeHead(204).end();
+        },
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params;
+
+            database.delete('users', id);
+
+            return res.writeHead(204).end();
+        },
     }
 ]
